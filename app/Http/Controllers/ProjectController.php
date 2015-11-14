19fa9a94c7584f7bffd36fdb091872e2a42434ab
Sebastiaan Franken <sfranken@sebastiaan-zenbook.timberwolfstraat.nl>
2015-11-14 18:48:04 +0100

    Kleine commit

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,15 +10,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use App\Project;
 
 class ProjectController extends Controller
 {
+
+	/**
+	 * Returns a view with a list containing all the projects owned
+	 * by the user.
+	 *
+	 * @return view
+	 */
 	public function getIndex()
 	{
+		$subdata = [
+			'projects' => Auth::user()->projects
+		];
+
+		$data = [
+			'subtitle' => 'Projectlijst',
+			'content' => view('project/index', $subdata)
+		];
+
+		return view('partials/layout', $data);
 	}
 
+	/**
+	 * Shows the "create new project" page
+	 *
+	 * @return view
+	 */
 	public function getCreate()
 	{
+		$data = [
+			'subtitle' => 'Nieuw project aanmaken',
+			'content' => view('project/create')
+		];
+
+		return view('partials/layout', $data);
 	}
 
 	public function postCreate(Request $request)
